7f54f13e456c0f74c3bd1dfff7581ab4fd131ad4
George Mshv <159710176+OdySoft@users.noreply.github.com>
2024-02-21 15:24:55 -0800

    big update

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
-from flask import Flask , render_template
+from flask import Flask , render_template , url_for , request
+from src.data import data
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+    print(request.endpoint)
     return render_template("index.html")
 
 @app.route("/shop")
 def shop():
-    return render_template('shop.html')
+    return render_template('shop.html' , products = data)
 
+@app.route("/product/<int:product_id>")
+def product(product_id):
+    return render_template("product.html", product = data[product_id])
 
 if __name__ == "__main__":
     app.run(debug=True)
