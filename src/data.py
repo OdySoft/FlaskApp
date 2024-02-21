325865c09a6af5178097efa35696a153433bdaa0
George Mshv <159710176+OdySoft@users.noreply.github.com>
2024-02-21 14:56:18 -0800

    data.py is used for fake database.
    This file will be used before adding the database

---
+++ src/data.py
@@ -0,0 +1,14 @@
+#fake database
+data = [
+    {"img" : "https://pcroom.ge/image/cache/catalog/2-1635-83-244x244.png",
+     "title": "დედაბარათი - Gigabyte B760M Gaming D4",
+     "price": "340₾"},
+
+     {"img" : "https://pcroom.ge/image/cache/catalog/2-1736-71-244x244.png",
+     "title": "დედაბარათი - Msi Pro B650M-P D5",
+     "price": "240₾"},
+
+      {"img" : "https://pcroom.ge/image/cache/catalog/2-7461-33-244x244.png",
+     "title": "დედაბარათი - Asus Rog Strix B760-A Gaming Wi-Fi D4",
+     "price": "740₾"}
+]
